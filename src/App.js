dd07df8354226c93710cb0d78d20d912deec2dd2
Sameh Mohamed <sameh.abdelhaleem95@gmail.com>
2021-03-14 21:22:12 +0200

    add the task adding function

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
-import React, {useState} from "react";
-
+import React, { useState } from "react";
+import "App.css";
+import TodoForm from "./components/todoForm";
 function App() {
   // state
-  const [todos,setTodos]=useState([])
+  const [todos, setTodos] = useState([]);
+
+  // functions
+  const addTodo = (todo) => {
+    setTodos([todo, ...todos]);
+  };
   return (
-    
+    <div className="App">
+      <header className="App-header">
+        <p>React Todo</p>
+        <TodoForm addTodo={addTodo}></TodoForm>
+      </header>
+    </div>
   );
 }
 
